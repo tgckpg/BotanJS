bbf4f47e8611b291924cc970dd6f60de91288f1a
斟酌 鵬兄 <tgckpg@gmail.com>
2016-03-16 19:03:06 +0800

    Remove the license header inside js files

--- botanjs/src/System/utils/Perf.js
+++ botanjs/src/System/utils/Perf.js
@@ -2,11 +2,6 @@
 	// Performance Functions
 	var ns = __namespace( "System.utils.Perf" );
 
-	/** {{{ Fast UUID generator, RFC4122 version 4 compliant.
-	 * author: Jeff Ward (jcward.com).
-	 * license: MIT license
-	 * link: http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
-	 **/
 	var lut = [];
 	for ( var i=0; i < 256; i++ )
 	{
