65100e652cf1efdedad004e4d5f60079ef1508fb
斟酌 鵬兄 <tgckpg@gmail.com>
2016-06-01 00:23:24 +0800

    Fix SiteRoot for rebuild

--- botan-rebuild.py
+++ botan-rebuild.py
@@ -5,6 +5,11 @@ sys.path.append( os.path.abspath( "." ) )
 from botanjs.service.jwork import app, JWork
 from botanjs.config import Config as config
 
+SiteRoot = os.path.abspath( "." )
+
+# Setting the SiteRoot for config
+config["Paths"]["SiteRoot"] = SiteRoot;
+
 bmap = os.path.join( config["Paths"]["Cache"], "botanjs", "bmap.xml" )
 
 app.conf.update( BROKER_URL = config["BotanJS"]["CeleryBroker"] )
